2f97753926c0ddd4148bec2ecec1e2555971185b
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2020-01-03 14:34:19 +0100

    Simplify property implementation and fix ctor visibility

--- AspectNetSharp/ObserverBaseAttribute.cs
+++ AspectNetSharp/ObserverBaseAttribute.cs
@@ -5,10 +5,8 @@ namespace ArwynFr.AspectNetSharp
     [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
     public abstract class ObserverBaseAttribute : Attribute
     {
-        private string _name;
+        protected ObserverBaseAttribute(string name) { Name = name; }
 
-        public ObserverBaseAttribute(string name) { _name = name; }
-
-        public string Name { get { return _name; } }
+        public string Name { get; }
     }
 }
