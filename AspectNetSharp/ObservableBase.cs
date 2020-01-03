7fc3bbb0516b143c0872509a80689c2106a48065
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2020-01-03 17:26:33 +0100

    Simplify Raise methods

--- AspectNetSharp/ObservableBase.cs
+++ AspectNetSharp/ObservableBase.cs
@@ -22,24 +22,12 @@ namespace ArwynFr.AspectNetSharp
 
         protected virtual void Dispose(bool disposing) { }
 
-        private void RaisePropertyChanging(string propertyName)
-        {
-            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
-        }
+        protected void RaisePropertyChanging(string propertyName) => PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
 
-        private void RaisePropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        protected void RaisePropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        private void RaiseOperationExecuting(string operationName)
-        {
-            OperationExecuting?.Invoke(this, new OperationExecutingEventArgs(operationName));
-        }
+        protected void RaiseOperationExecuting(string operationName) => OperationExecuting?.Invoke(this, new OperationExecutingEventArgs(operationName));
 
-        private void RaiseOperationExecuted(string operationName)
-        {
-            OperationExecuted?.Invoke(this, new OperationExecutedEventArgs(operationName));
-        }
+        protected void RaiseOperationExecuted(string operationName) => OperationExecuted?.Invoke(this, new OperationExecutedEventArgs(operationName));
     }
 }
